f9b58c47a1ad5542e664dedee83744455efb6f62
shashankBgowda <shashankbg74@gmail.com>
2025-03-08 08:44:12 -0500

    http route setup

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 import { PORT } from "./config.js";
 
 const app = express();
+app.get('/',(request, response)=>{
+    console.log(request);
+    return response.status(234).send("welcome brun..!")
+})
 
 app.listen(PORT, ()=>{
     console.log(`App is listning to your port number ${PORT}`);
